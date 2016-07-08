4736df08730858da2d027a49b68f04d59269aea5
len.li <652231946@qq.com>
2016-07-08 18:13:46 +0800

    add buildmodel

---
+++ clown-code/src/main/java/com/clown/code/model/BuildModel.java
@@ -0,0 +1,41 @@
+package com.clown.code.model;
+
+/**
+ * Created by lenli on 2016/7/8.
+ *
+ * @Author Libin
+ * @Date 2016/7/8
+ */
+public class BuildModel {
+
+    /**
+     * 项目根包
+     */
+    private String packageName;
+
+    /**
+     * 实体包路径
+     */
+    private String packageNameModel;
+
+    /**
+     * dao接口包路径
+     */
+    private String packageNameDao;
+
+    /**
+     * Service 层包路径
+     */
+    private String packageNameService;
+
+    /**
+     * Controller层包路径
+     */
+    private String packageNameController;
+    /**
+     * 作者名称
+     */
+    private String authorName;
+
+
+}
